af4a18c32a408ca932ffbdd3853bd234e87f48f3
jumpman22 <hiram.cho@gmail.com>
2018-02-06 20:58:54 -0800

    init repo

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var milesDriven = window.prompt("How many miles have you driven?");
+var gallons = window.prompt("How many gallon of fuel does your car hold?");
+var mpg = milesDriven / gallons;
+document.write(parseInt(mpg, 10));
+
